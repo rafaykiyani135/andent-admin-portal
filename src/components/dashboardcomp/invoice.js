0ef9df1ee8b071d570bb822cd6a38b75719803af
Aashar Mehmood <aasharmehmood3@gmail.com>
2023-12-07 17:34:22 +0500

    invoice multiple file upload

--- src/components/dashboardcomp/invoice.js
+++ src/components/dashboardcomp/invoice.js
@@ -6,6 +6,7 @@ import mailicon from "../../assets/data/mailicon.png";
 import excl from "../../assets/data/exclamation.png";
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthProvider";
+import { imageExtensions } from "../../constants/index";
 import {
   uploadClientFile,
   deleteClientFile,
@@ -13,18 +14,25 @@ import {
 } from "../../services/api/clients";
 import { toast } from "react-toastify";
 import DeleteModal from "../modals/DeleteModal";
-
+import { getFileExtension } from "../../services/helperFunctions";
+import PdfPreview from "../modals/PdfPreview";
+import ImagePreview from "../modals/ImagePreview";
 function Invoice(props) {
   const { id } = props.clientInvoiceData;
   const { setInvoiceOpen } = props;
 
   const { user } = useContext(AuthContext);
-  const [file, setFile] = useState("");
-  const [fileName, setFileName] = useState("");
+  const [invoiceIndex, setInvoiceIndex] = useState(0);
+  const [invoiceId, setInvoiceId] = useState(null);
   const [uploadingInvoice, setUploadingInvoice] = useState(false);
-  const [uploadedInvoiceId, setUploadedInvoiceId] = useState("");
+  const [sendingInvoice, setSendingInvoice] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [email, setEmail] = useState("");
+  const [invoiceFiles, setInvoiceFiles] = useState([]);
+  const [previewImage, setPreviewImage] = useState(false);
+  const [previewPdf, setPreviewPdf] = useState(false);
+  const [previewFileName, setPreviewFileName] = useState("");
+  const [previewFileUrl, setPreviewFileUrl] = useState("");
   const [desc, setDesc] = useState(
     "Dear Sir/Ma'am,\n\nKindly review the attached invoice and let us know if you have any queries.\n\nBest Regards,\nAndent Clinic."
   );
@@ -33,7 +41,22 @@ function Invoice(props) {
     setEmail(props.clientInvoiceData.email);
   }, [props.clientInvoiceData]);
 
+  const handleFilePreview = (fileData) => {
+    const { name, url } = fileData;
+
+    if (imageExtensions.includes(getFileExtension(name))) {
+      setPreviewImage(true);
+    } else if (getFileExtension(name) === "pdf") {
+      setPreviewPdf(true);
+    }
+
+    setPreviewFileName(name);
+    setPreviewFileUrl(url);
+  };
+
   const handleInvoiceUpload = (e) => {
+    setUploadingInvoice(true);
+    const files = e.target.files;
     const allowedExtensions = [
       "pdf",
       "jpg",
@@ -44,52 +67,86 @@ function Invoice(props) {
       "JPEG",
       "PNG",
     ];
-    const fileExtension = e.target.files[0].name.split(".").pop().toLowerCase();
 
-    if (allowedExtensions.includes(fileExtension)) {
-      const invoiceFile = e.target.files[0];
-      setUploadingInvoice(true);
-      const payLoad = {
-        clientId: id,
-        file: invoiceFile,
-        type: "INVOICE",
-        userId: user.id,
-      };
-      uploadClientFile(payLoad)
-        .then((res) => {
+    // Create an array to store promises for each file upload
+    const uploadPromises = [];
+    const responses = [];
+    let anyInvalidFile = false;
+    // Iterate over each file and create a payload for each
+    for (const file of files) {
+      const fileExtension = getFileExtension(file.name);
+      if (allowedExtensions.includes(fileExtension)) {
+        const payLoad = {
+          clientId: id,
+          file,
+          type: "INVOICE",
+          userId: user.id,
+        };
+        const uploadPromise = uploadClientFile(payLoad)
+          .then((res) => {
+            responses.push(res.data.data); // Capture the response for each file
+          })
+          .catch((err) => {
+            responses.push(null);
+          });
+
+        uploadPromises.push(uploadPromise);
+      } else {
+        setUploadingInvoice(false);
+        toast.error("Supported file formats : png,jpg,jpeg & pdf");
+        anyInvalidFile = true;
+      }
+    }
+
+    console.log(anyInvalidFile, uploadPromises.length);
+
+    if (anyInvalidFile && uploadPromises.length > 1) {
+      toast.success("Files with valid format are uploaded");
+    } else if (anyInvalidFile && uploadPromises.length === 1) {
+      toast.success("File with valid format is uploaded");
+    } else if (!anyInvalidFile && uploadPromises.length > 1) {
+      toast.success("All files uploaded Successfully");
+    } else if (!anyInvalidFile && uploadPromises.length === 1) {
+      toast.success("File Uploaded Successfully");
+    }
+
+    uploadPromises.length > 0 &&
+      Promise.all(uploadPromises)
+        .then(() => {
+          // All files uploaded successfully
           setUploadingInvoice(false);
-          setFile(res?.data?.data?.url);
-          setFileName(res?.data?.data?.name);
-          setUploadedInvoiceId(res?.data?.data?.id);
-          toast.success("Invoice Uploaded");
+          setInvoiceFiles((prevFiles) => [...prevFiles, ...responses]);
         })
         .catch((err) => {
+          // Handle any error that occurred during the parallel uploads
+          setInvoiceFiles((prevFiles) => [...prevFiles, ...responses]);
           setUploadingInvoice(false);
           toast.error(
-            err?.response?.data?.message ?? "Failed to upload invoice"
+            err?.response?.data?.message ||
+              err.message ||
+              "Failed to upload one or more files"
           );
         });
-    } else {
-      toast.error("Supported file formats : png,jpg,jpeg & pdf");
-    }
   };
+
+  useEffect(() => {
+    console.log(invoiceFiles);
+  }, [invoiceFiles]);
+
   const delInvoice = () => {
-    document.getElementById("my-file").value = "";
     setShowModal(false);
-    if (uploadedInvoiceId) {
-      deleteClientFile(uploadedInvoiceId)
-        .then((res) => {
-          toast.success("Invoice deleted succcessfully");
-          setFile(null);
-          setFileName("");
-          setUploadedInvoiceId("");
-        })
-        .catch((err) => {
-          toast.error(
-            err?.response?.data?.message ?? "Failed to delete invoice"
-          );
+    deleteClientFile(invoiceId)
+      .then((res) => {
+        toast.success("Invoice deleted successfully");
+        setInvoiceFiles((prevFiles) => {
+          const updatedFiles = [...prevFiles];
+          updatedFiles.splice(invoiceIndex, 1); // Remove the file at the specified index
+          return updatedFiles;
         });
-    }
+      })
+      .catch((err) => {
+        toast.error(err?.response?.data?.message ?? "Failed to delete Invoice");
+      });
   };
 
   function handleInvoiceSending() {
@@ -97,7 +154,7 @@ function Invoice(props) {
       toast.error("Please enter description");
       return;
     }
-    if (!file) {
+    if (!invoiceFiles) {
       toast.error("Please upload invoice file");
       return;
     }
@@ -106,19 +163,18 @@ function Invoice(props) {
       subject: "Andent incoive for your dental procedure",
       body: desc,
       clientId: id,
-      clientFileId: uploadedInvoiceId,
-      url: file,
+      clientFileIds: invoiceFiles?.map((file) => file?.id),
       userId: user.id,
     };
+    setSendingInvoice(true);
     sendInvoiceToClient(payLoad)
       .then((res) => {
+        setSendingInvoice(false);
         toast.success("Invoice Sent to Client");
-        setFile("");
-        setFileName("");
-        setUploadedInvoiceId("");
         setInvoiceOpen(false);
       })
       .catch((err) => {
+        setSendingInvoice(false);
         toast.error(
           err?.response?.data?.message ?? "Failed to send the invoice"
         );
@@ -189,30 +245,37 @@ function Invoice(props) {
           ></textarea>
         </div>
       </div>
-      {file ? (
-        <div className="row justify-content-start" style={{ width: "100%" }}>
-          <div className="col-12 col-lg-12 text-start">
-            <h2 className="popup-heading-3 text-start d-flex align-items-center">
-              {fileName}
-              <a href={file} download={file} style={{ marginLeft: "10px" }}>
-                <img
-                  src={download}
-                  alt="download-icon"
-                  className="small-icon"
-                />
-              </a>
-              <span
-                style={{ marginLeft: "10px", cursor: "pointer" }}
-                onClick={() => setShowModal(true)}
-              >
-                <img src={del} alt="delete-icon" className="small-icon" />
-              </span>
-            </h2>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+      {invoiceFiles?.map((file, index) => (
+        <h2
+          key={index}
+          className="popup-heading-3 text-start d-flex align-items-center justify-content-start"
+        >
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => handleFilePreview(file)}
+          >
+            {file?.name}
+          </span>
+
+          <a
+            href={file?.url}
+            download={file?.url}
+            style={{ marginLeft: "10px" }}
+          >
+            <img src={download} alt="download-icon" className="small-icon" />
+          </a>
+          <span
+            style={{ marginLeft: "10px" }}
+            onClick={() => {
+              setInvoiceIndex(index);
+              setInvoiceId(file?.id);
+              setShowModal(true);
+            }}
+          >
+            <img src={del} alt="delete-icon" className="small-icon" />
+          </span>
+        </h2>
+      ))}
       <div
         className="row justify-content-start d-flex"
         style={{ width: "100%" }}
@@ -221,7 +284,7 @@ function Invoice(props) {
           className="col-12 col-lg-12 text-start d-flex justify-content-center"
           style={{ gap: "24px" }}
         >
-          <label className={`andent-button ${file ? `button-disabled` : ``}`}>
+          <label className="andent-button">
             <h2 className="button-text d-flex text-center">
               {uploadingInvoice ? "Uploading" : "Upload Invoice"}
             </h2>
@@ -230,6 +293,7 @@ function Invoice(props) {
             </span>
             <input
               id="my-file"
+              multiple
               onChange={handleInvoiceUpload}
               type="file"
               style={{ display: "none" }}
@@ -243,7 +307,9 @@ function Invoice(props) {
           </label>
 
           <button className="andent-button" onClick={handleInvoiceSending}>
-            <h2 className="button-text">Send Invoice</h2>
+            <h2 className="button-text">
+              {sendingInvoice ? "Sending ..." : "Send Invoice"}
+            </h2>
             <span className="d-flex align-items-center">
               <img src={invwhite} alt="genlink icon" className="small-icon" />
             </span>
@@ -255,6 +321,22 @@ function Invoice(props) {
           setShowModal={setShowModal}
           modalDescription={"Are you sure you want to delete this file?"}
           onConfirm={delInvoice}
+        />
+        <ImagePreview
+          {...{
+            previewImage,
+            setPreviewImage,
+            previewFileName,
+            previewFileUrl,
+          }}
+        />
+        <PdfPreview
+          {...{
+            previewPdf,
+            setPreviewPdf,
+            previewFileName,
+            previewFileUrl,
+          }}
         />
       </div>
     </>
